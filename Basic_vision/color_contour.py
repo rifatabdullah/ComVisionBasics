abb639d787cc971268308746262dca02f3f8c9c6
levia <rifatabdullah754@gmail.com>
2026-07-12 15:16:51 +0600

    c

--- Basic_vision/color_contour.py
+++ Basic_vision/color_contour.py
@@ -1,13 +1,18 @@
 import cv2
 import cvzone
 from cvzone.ColorModule import ColorFinder
+from cvzone.Utils import findContours
 
 
 cap = cv2.VideoCapture(0)
 colorfin = ColorFinder(trackBar= False) # for searching color 
 
 
-hsvVal = {'hmin': 17, 'smin': 29, 'vmin': 0, 'hmax': 56, 'smax': 253, 'vmax': 255} # body spray 
+# hsvVal = {'hmin': 0, 'smin': 37, 'vmin': 95, 'hmax': 52, 'smax': 255, 'vmax': 255}
+
+
+hsvVal = {'hmin': 17, 'smin': 29, 'vmin': 0, 'hmax': 56, 'smax': 253, 'vmax': 255} # body spr
+
 
 while True:
     flag, img = cap.read()
@@ -16,8 +21,11 @@ while True:
         break 
     
     imgCol, mask = colorfin.update(img,hsvVal) 
+    imgCon, conFound = findContours(img,mask)
+    if conFound:
+        print(conFound[0]['center'])
     
-    imgStack = cvzone.stackImages([img,imgCol,mask],3,0.5)
+    imgStack = cvzone.stackImages([img,imgCol,mask,imgCon],4,0.5)
     
     
     cv2.imshow("Color Detection", imgStack)
